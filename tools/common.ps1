0df5d95b4857be9b3b8e9cd05dad87df5c8c950c
Owen Smith <owen.smith@citrix.com>
2018-10-05 15:39:18 +0100

    Initial Commit: SDK 1809 (10.0.17763)
    Signed-off-by: Owen Smith <owen.smith@citrix.com>

---
+++ tools/common.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'WindowsSDK10'
+$installerType = 'EXE'
+$url = 'https://download.microsoft.com/download/9/3/9/939441D4-6FBA-48EE-9EF7-402C1AA8B8A7/windowssdk/winsdksetup.exe'
+$silentArgs = '/features + /q /norestart'
+$validExitCodes = @(0)
+$checksum = '4E45C1AA8177675E40F8EC85E121D0CF44C2E00A'
+$checksumType = 'sha1'
